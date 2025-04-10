2df9813fbe902f9223ce5cee47ebd3bec88def64
Ashish Rawat <144007425+Ashish252003@users.noreply.github.com>
2025-04-10 12:12:40 +0530

    Add files via upload
    A simple and responsive Employee Administration Portal built with Blazor and MudBlazor, using SQLite as the backend database. This project allows admin users to manage employee records efficiently through a modern UI and lightweight architecture.
    
    🔧 Tech Stack
    Blazor Server (.NET)
    
    MudBlazor (UI Components)
    
    SQLite (Database)
    
    Entity Framework Core (ORM)

---
+++ Controllers/EmployessController.cs
@@ -0,0 +1,105 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Controllers
+{  //localhost /api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployessController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployessController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+
+
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+
+
+        [HttpPut]
+        [Route("{id ;guid}")]
+        public IActionResult UpdateEmplyee(Guid id,UpdateEmployeedto updateEmployeedto)
+        
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeedto.Name;
+            employee.Email = updateEmployeedto.Email;
+            employee.Phone = updateEmployeedto.Phone;
+            employee.Salary=updateEmployeedto.Salary;
+
+
+            dbContext.SaveChanges();
+
+            return Ok(employee);
+
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee=dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Employees.Remove(employee);
+
+            dbContext.SaveChanges();
+        
+           return Ok();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+//public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+//{
+//    public ApplicationDbContext CreateDbContext(string[] args)
+//    {
+//        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+//        optionsBuilder.UseSqlServer("Your_Connection_String_Here");
+
+//        return new ApplicationDbContext(optionsBuilder.Options);
+//    }
+//}
+
